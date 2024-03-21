3295811135bffea5f7988a26cf8ed2bbbeff90ff
kshuta <shutakumada@gmail.com>
2024-03-21 01:57:12 -0500

    added fish and lua

---
+++ .config/fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    fish_vi_key_bindings
+    alias config="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
+    
+end
