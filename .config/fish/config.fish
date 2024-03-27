02d29e157c66e9077dfdd5416f79f2721c4c0e08
kshuta <shutakumada@gmail.com>
2024-03-27 12:28:36 -0500

    added keybindings for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_vi_key_bindings
     alias config="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
-    
+    bind -M insert \cy accept-autosuggestion
+    bind -M insert \cn forward-word
 end
